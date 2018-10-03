4b014c0c1ad9364739768d4751233d7b57048417
William Kulha <kulha.william@gmail.com>
2018-10-03 14:00:02 -0400

    Cleaned up the code and made comments more meaningful.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
 //Back-end Logic
-
 //Store the message for a positive outcome as a variable.
-var isPalindromeMessage = "Wow! Now <em>that's </em> what I call a palindrome!"
+var isPalindromeMessage = "Yes! Yes!<br>You've done it!<br>Wow! Now <em>that's </em> what I call a palindrome!"
 //store the message for a negative outcome as a variable.
 var notPalindromeMessage = "Boooo! We're sorry, but that's <em> not </em>a palindrome!"
 //Define a custom function to remove punctuation marks.
 function punctuationless(stringToStrip) {
   return stringToStrip.replace(/[^A-z0-9_]/g,"");
 }
-
 function reversedString(string) {
   let lettersAsArray = string.split("");
   return lettersAsArray.reverse().join("");
 }
-
-//Define a custom function to take user's input string, turn it into an array, then turn that array
-//into an array containing single characters. Save this array for later comparison!
+//A custom function that strips the original string, saves it for comparison, converts it into an array
+//then reverses that array before joining it back into a string to compare with the original string.
+//The results of this comparison are printed in the output div on the document.
 function stringAnalyzer(string) {
   let wordArray = []
   let cleanString = punctuationless(string);
   let original = cleanString;
   let reversed = reversedString(cleanString);
   $('#reversed_input').text($('#user_string').val());
-
-  console.log(cleanString, reversed);
-
+  if (original === reversed) {
+    $('#palindrome_results').empty();
+    $('#palindrome_results').html(isPalindromeMessage);
+  } else if (original != reversed) {
+    $('#palindrome_results').empty();
+    $('#palindrome_results').html(notPalindromeMessage);
+  }
 }
-//define function to compare the indices of both arrays to see if there is no difference.
-
-
 //Front-end Logic
 $(document).ready(function() {
   $('#user_input').submit(function(event) {
     event.preventDefault();
     stringAnalyzer($('#user_string').val());
-  })
-})
+  });
+});
